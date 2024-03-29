595c9b91c22c744fc2dfc017a313b09ea6817cd4
SzyWoj <148621843+SzyWoj@users.noreply.github.com>
2024-03-29 12:52:38 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace StaticModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Ferrari");
+            Car car2 = new Car("Porsche");
+            Car car3 = new Car("Ford");
+            
+            Console.WriteLine(Car.numberOfCars);
+
+            Car.StartRace();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        string model;
+        public static int numberOfCars;
+
+        public Car(string model)
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+
+        public static void StartRace()
+        {
+            Console.WriteLine("The race has begun!");
+        }
+    }
+}
